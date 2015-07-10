ddbefe4012bcd300e260160c310a89f493c1c580
Mary Grace Lucas <marygracelucas1@Marys-MacBook-Pro.local>
2015-07-10 16:36:22 -0400

    First commit includes long form of show size converter app code

--- ShoeSizeConverter/ViewController.swift
+++ ShoeSizeConverter/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,43 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        //Definies a variable called shoe size and pulls it from the mensShoeSizeTextField
+        let sizeFromTextField = mensShoeSizeTextField.text
+        
+        //Converts shoe size from a string into an integer so it can be added with the constant (matching the types)
+        let numberFromTextField = sizeFromTextField.toInt()
+        
+        //Adding an exclaimation point force unwraps the optional integer; it's no longer an optional interger
+        var integerFromTextField = numberFromTextField!
+        
+        //Defines the constant as 30
+        let conversionConstant = 30
+        
+        //Adds shoe size and constant
+        integerFromTextField += conversionConstant
+        
+        //Unhides label after the button is pressed
+        mensConvertedShoeSizeLabel.hidden = false
+        
+        //Converts the sum into a string so it can be displayed in the label
+        let stringWithUpdatedShoeSize = "\(integerFromTextField)"
+        
+        //Sets the label text to the text of the new string holding the update shoe size
+        mensConvertedShoeSizeLabel.text = stringWithUpdatedShoeSize
+        
+        //Tucks keyboard away after hitting button
+        mensShoeSizeTextField.resignFirstResponder()
+        
+        //Empties text field after user presses button
+        mensShoeSizeTextField.text = ""
+        
+        
+        
+    }
 
 }
 
